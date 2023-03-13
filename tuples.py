010eb76970e9936c3e6ef56b1dbf61b70af95876
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2023-03-13 21:17:56 +0100

    add tuples

---
+++ tuples.py
@@ -0,0 +1,22 @@
+# Tuples - imutable #
+
+# my_tuple = ('a', 'p', 'p', 'l', 'e')
+# print(my_tuple.count("p")) # 2
+# print(my_tuple.index("a")) # 0
+# my_list = list(my_tuple) # ['a', 'p', 'p', 'l', 'e']
+# my_tuple2 = tuple(my_list) # ('a', 'p', 'p', 'l', 'e')
+
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(a[2:5]) # (3, 4, 5)
+# print(a[::2]) # (1, 3, 5, 7, 9)
+# print(a[::-1]) # (10, 9, 8, 7, 6, 5, 4, 3, 2, 1) (trick to reverse)
+
+"""
+# Unpacking (~ destructuring) #
+my_tuple = "Max", 28, "Boston"
+name, age, city = my_tuple
+print(name) # Max
+print(age) # 28
+print(city) # Boston
+"""
+
